a5d990b45e16d2337e6f43c86f106d187ca0ffa4
Karoly <70485534+Karoly-Git@users.noreply.github.com>
2020-11-22 00:05:01 +0000

    Update finding_max_min_in_list.py

--- finding_max_min_in_list.py
+++ finding_max_min_in_list.py
@@ -3,7 +3,7 @@
 list = [23, 0.81, 88.2, 18, -4.6, 71]
 
 list_max = list[0]                                          #  let's suppose that the first element is the greatest, it might or might not be true
-for i in range(len(list)):                                  #  let's go through the list one by one and examine if the particular element is greater than the first / previous element
+for i in range(len(list)):                                  #  let's go through the list one by one and examine if the particular element is greater than the first / previous one
     if list[i] > list_max:
         list_max = list[i]                                  #  if if the particular element is greater than any of the previous element then change it
 print("The greats number in the list:\n", list_max, sep="")
